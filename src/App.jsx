99268c4dac4891ed6b723367c06514fee6d9ba92
faridhakanda <faridakanda1729@gmail.com>
2026-03-31 23:02:31 +0600

    header section is partially completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 
 import './App.css'
+import Footer from './components/footer'
+import Header from './components/header'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className="">
+        <Header />
         <h2 >Hello, World</h2>
         <button className='btn  btn-secondary btn-outline'>Click Me!</button>
         <h2 className='text-center items-center mx-auto text-3xl my-auto bg-yellow-400 text-lime-500'>Digital Tools Project for Assignment-06!</h2>
     
         <button className='btn btn-outline btn-primary'>Farid Akanda</button>
     
-        <button className="btn" onclick="my_modal_2.showModal()">open modal</button>
-        <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-            <h3 className="text-lg font-bold">Hello!</h3>
-            <p className="py-4">Press ESC key or click outside to close</p>
-        </div>
-        <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-        </form>
-        </dialog>
+        <Footer />
     </div>
   )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ShopingCart from "../assets/products/shopping-cart.png"
+const Header = () => {
+    return (
+        <div className='shadow-sm '>
+            <div className='flex justify-around py-2  items-center w-[1200px]  mx-auto'>
+                <div>
+                    <h2 className='bg-gradient-to-l from-[#9514FA] to-[#4F39F6] text-3xl font-bold bg-clip-text text-transparent'>DigiTools</h2>
+                </div>
+                <div className=''>
+                    <ul className='flex space-x-3 font-semibold text-[#101727]'>
+                        <li>Products</li>
+                        <li>Features</li>
+                        <li>Pricing</li>
+                        <li>Testimonials</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+                <div className='flex  text-[#101727] font-semibold z-1 space-x-6 p-2'>
+                    <div className='flex space-x-2 items-center'>
+                        <img className='' src={ShopingCart} alt="Shoping cart Bucket" />
+                        <p className='z-10 absolute text-red-500 -mt-5 ml-1 text-sm'>4</p>
+                        <h2>Login</h2>
+                    </div>
+                    <h2 className='bg-gradient-to-l from-[#9514FA] to-[#4F39F6] text-[#FFFFFF] px-2 py-1 rounded-3xl'>Get Started</h2>
+                </div>
+                
+            </div>
+        </div>
+        
+    );
+};
+
+export default Header;
