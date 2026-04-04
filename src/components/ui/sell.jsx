fad823a9cb2ffaa6065f218ca262d541487ef2c7
faridhakanda <faridakanda1729@gmail.com>
2026-04-04 09:48:40 +0600

    sell banner is ready

--- src/components/ui/sell.jsx
+++ src/components/ui/sell.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Sell = () => {
     return (
-        <div className='bg-gradient-to-l from-[#9514FA] to-[#4F39F6] mb-12'>
+        <div className='bg-gradient-to-l from-[#9514FA] to-[#4F39F6] mb-12 py-12'>
             <div className='flex justify-between w-[1200px] mx-auto py-6 '>
                 <div className='mx-auto'>
                     <h2 className='font-extrabold text-[60px] text-[#FFFFFF]'>50K+</h2>
